83929d7da1ebc6112fb03c92fe6c3c434ab381b4
HarmanpreetSingh588 <harmandhami78@gmail.com>
2025-08-14 13:27:51 -0400

    Update Category.js

--- src/models/Category.js
+++ src/models/Category.js
@@ -13,21 +13,3 @@
 
 // export default mongoose.models.Category || mongoose.model("Category", categorySchema);
 
-
-import mongoose from "mongoose";
-
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Category name is required"],
-    trim: true,
-    minlength: [2, "Category name must be at least 2 characters"],
-    maxlength: [50, "Category name must not exceed 50 characters"],
-    unique: true,
-  },
-});
-
-// Check if the model already exists in mongoose.models
-const Category = mongoose.models.Category || mongoose.model("Category", categorySchema);
-
-export default Category;
