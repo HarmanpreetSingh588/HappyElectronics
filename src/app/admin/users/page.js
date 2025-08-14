9a7d69f2586b01e404441812005f306c1624ce7a
HarmanpreetSingh588 <harmandhami78@gmail.com>
2025-08-14 13:14:05 -0400

    Update page.js

--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -1,7 +1,7 @@
-import fetchUsers from "@/app/actions/fetchUsers";
-import AdminUsersClient from "@/Components/AdminUsersClient";
+// import fetchUsers from "@/app/actions/fetchUsers";
+// import AdminUsersClient from "@/Components/AdminUsersClient";
 
-export default async function AdminUsersPage() {
-  const users = await fetchUsers();
-  return <AdminUsersClient initialUsers={users} />;
-}
+// export default async function AdminUsersPage() {
+//   const users = await fetchUsers();
+//   return <AdminUsersClient initialUsers={users} />;
+// }
