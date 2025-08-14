bcd58aee6d4e99bee39b2a9dce8cd221e53d27d9
HarmanpreetSingh588 <harmandhami78@gmail.com>
2025-08-14 13:11:01 -0400

    Update page.js

--- src/app/admin/categories/page.js
+++ src/app/admin/categories/page.js
@@ -6,20 +6,4 @@
 //   return <AdminCategoriesClient initialCategories={categories} />;
 // }
 
-import fetchCategories from "@/app/actions/fetchCategories";
-import AdminCategoriesClient from "@/Components/AdminCategoriesClient";
 
-// Force this page to run dynamically on each request
-export const dynamic = "force-dynamic";
-
-export default async function AdminCategoriesPage() {
-  let categories = [];
-
-  try {
-    categories = await fetchCategories();
-  } catch (error) {
-    console.error("Failed to fetch categories:", error);
-  }
-
-  return <AdminCategoriesClient initialCategories={categories} />;
-}
